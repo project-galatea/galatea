456cf8f2ea9bd7370267e337d886d01b6377e237
nacgarg <nachiketa.gargi@gmail.com>
2016-04-20 15:09:26 -0700

    Cleaned up some code

--- dataset.py
+++ dataset.py
@@ -2,17 +2,24 @@
 import os
 import numpy as np
 class Dataset():
+	def __init__(self, logger):
+		self._logger = logger
+
+		self._logger.info("Dataset init.")
+
 	def get_csvs(self,path):
 		content_list = []
 		for content in os.listdir(path): # "." means current directory
 			if content.endswith(".csv"):
 				content_list.append(path+content)
 		return content_list
+
 	def load_csv(self,path):
 		with open(path) as g:
 			stuff=g.readlines()
 		lines=[line[:-1] for line in stuff[1:]]#cut off the \n at the end of each one, and also cut off the first line which is a csv header
 		return [self.parseline(line) for line in lines]
+
 	def parseline(self,line):
 		splitted=line.split(",")
 		usernameandtime=splitted[-2:]
@@ -21,8 +28,10 @@ class Dataset():
 		otherstuff=",".join(splitted[:-2])#there might have been commas in the first part, so put those back
 		otherstuff=otherstuff[1:-1].lower()
 		return {'Msg': otherstuff, 'User':username, 'Date':date}
+
 	def converttosamples(self,lines):
 		return [self.sample(line) for line in lines]
+
 	def indexfromchar(self,char):
 		if(char==' '):
 			return 1
@@ -30,6 +39,7 @@ class Dataset():
 		if(o>=ord('a') and o<=ord('z')):
 			return o-ord('a')+3
 		return 2
+
 	def sample(self,line):
 		max_chars_per_msg=10
 		temp=np.zeros((max_chars_per_msg+1,29),dtype="bool")
@@ -41,29 +51,27 @@ class Dataset():
 		else:
 			temp[len(line['Msg'])-1][0]=1
 		return temp
+
 	def load_csvs_from_folder(self,path):
-		csvs=self.get_csvs(path)
-		X=np.array([],dtype="bool")
-		num_msgs_to_concat=2
-		max_chars_per_msg=10
-		X.shape=(0,(max_chars_per_msg+1)*num_msgs_to_concat,29)
-		Y=np.array([],dtype="bool")
-		Y.shape=(0,max_chars_per_msg+1,29)
+		csvs = self.get_csvs(path)
+		num_msgs_to_concat = 2 #TODO move these to config
+		max_chars_per_msg = 10
+		X = np.zeros((0,(max_chars_per_msg+1)*num_msgs_to_concat,29), dtype="bool")
+		Y = np.zeros((0,max_chars_per_msg+1,29), dtype="bool")
+
 		for csv in csvs:
-			lines=self.load_csv(csv)
-			samples=self.converttosamples(lines)
+			lines = self.load_csv(csv)
+			samples = self.converttosamples(lines)
 			for i in range(0,len(samples)-num_msgs_to_concat-1,1):
-				concatted=np.array([],dtype="bool")
-				concatted.shape=(0,29)
+				concatted = np.zeros((0, 29),dtype="bool")
 				for j in range(i,i+num_msgs_to_concat,1):
-					concatted=np.concatenate([concatted,samples[j]])
-				X=np.append(X,concatted)
-				Y=np.append(Y,samples[i+num_msgs_to_concat])
-		print X[0]
-		print Y[0]
-	def __init__(self):
-		print "INIT"
+					concatted = np.concatenate([concatted,samples[j]])
+				X = np.append(X,concatted)
+				Y = np.append(Y,samples[i+num_msgs_to_concat])
+
+		self._logger.debug(str(X[0]))
+		self._logger.debug(str(Y[0]))
 
 if __name__=="__main__":
 	d = Dataset()
-	d.load_csvs_from_folder("/Users/leijurv/Downloads/dank/")
+	d.load_csvs_from_folder(process.argv[1])
