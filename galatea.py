bb2b16336b06451ea963a7a71b91983d1f67784b
nacgarg <nachiketa.gargi@gmail.com>
2016-04-20 15:33:07 -0700

    Fixed typo

--- galatea.py
+++ galatea.py
@@ -4,7 +4,7 @@ from nn import LSTMNet
 class Galatea():
 	def __init__(self):
 		self._logger = L("test.log", 1)
-		self.net = LSTMNet(self.L)
+		self.net = LSTMNet(self._logger)
 		self.net.load_dataset()
 
 g = Galatea()
