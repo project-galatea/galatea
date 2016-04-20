cfd4b284efb486302d182a9c396bf57cf8fefaf5
Leif Jurvetson <leijurv@gmail.com>
2016-04-20 14:22:47 -0700

    updated

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 
 import os
+import numpy as np
 class Dataset():
 	def get_csvs(self,path):
 		content_list = []
@@ -11,8 +12,6 @@ class Dataset():
 		with open(path) as g:
 			stuff=g.readlines()
 		lines=[line[:-1] for line in stuff[1:]]#cut off the \n at the end of each one, and also cut off the first line which is a csv header
-		print lines[0]
-		print lines[-1]
 		return [self.parseline(line) for line in lines]
 	def parseline(self,line):
 		splitted=line.split(",")
@@ -20,11 +19,28 @@ class Dataset():
 		username=splitted[-1]
 		date=splitted[-2]
 		otherstuff=",".join(splitted[:-2])#there might have been commas in the first part, so put those back
-		return {'Msg': otherstuff[1:-1], 'User':username, 'Date':date}
+		otherstuff=otherstuff[1:-1].lower()
+		return {'Msg': otherstuff, 'User':username, 'Date':date}
+	def converttosamples(self,lines):
+		return [self.sample(line) for line in lines]
+	def indexfromchar(self,char):
+		if(char==' '):
+			return 0
+		o=ord(char)
+		if(o>=ord('a') and o<=ord('z')):
+			return o-ord('a')+2
+		return 1
+	def sample(self,line):
+		max_chars_per_msg=10
+		temp=np.zeros((max_chars_per_msg,28),dtype="bool")
+		for i in range(max_chars_per_msg):
+			if(i<len(line['Msg'])):
+				temp[i][self.indexfromchar(line['Msg'][i])]=1
 	def load_csvs_from_folder(self,path):
 		csvs=self.get_csvs(path)
 		for csv in csvs:
-			self.load_csv(csv)
+			lines=self.load_csv(csv)
+			self.converttosamples(lines)
 	def __init__(self):
 		print "INIT"
 
