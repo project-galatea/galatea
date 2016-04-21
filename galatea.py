8f9205135f376f2cfc0d5a53799bd99db9bb3017
nacgarg <nachiketa.gargi@gmail.com>
2016-04-20 23:51:52 -0700

    Moved functions out of init

--- galatea.py
+++ galatea.py
@@ -5,9 +5,9 @@ class Galatea():
 	def __init__(self):
 		self._logger = L("test.log", 1)
 		self.net = LSTMNet(self._logger)
-		self.net.load_dataset()
-		self.net.build_model()
-		self.net.load_weights()
+		# self.net.load_dataset()
+		# self.net.build_model()
+		# self.net.load_weights()
 
 	def train(self):
 		self.net.train()
@@ -18,6 +18,9 @@ class Galatea():
 
 def main():
 	g = Galatea()
+	g.net.load_dataset()
+	g.net.build_model()
+	g.net.load_weights()
 	g.train()
 	print g.generate(["hello", "how are you"])
 
